52ee0962c9c80f20cba90df2fb630d4026d6bcd0
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2023-02-04 14:35:24 -0800

    shopping cart html file done

---
+++ checkout.js
@@ -0,0 +1,15 @@
+//Cart
+
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+
+//Open and Close cart
+cartIcon.onclick = () => {
+    cart.classList.add("active");
+}
+
+closeCart.onclick = () => {
+    cart.classList.remove("active");
+}
