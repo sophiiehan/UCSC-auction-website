24f57b88acc6e8d50b76b6f31ab030bd146ca426
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2023-02-04 18:44:26 -0800

    Updated Style and started working on JS portion
    AS OF NOW: when you click the remove button, it deletes the item in your cart. the total does not yet change. currently working on it.

--- checkout.js
+++ checkout.js
@@ -1,15 +1,39 @@
-//Cart
 
-let cartIcon = document.querySelector('#cart-icon');
-let cart = document.querySelector('.cart');
-let closeCart = document.querySelector('#close-cart');
-
-
-//Open and Close cart
-cartIcon.onclick = () => {
-    cart.classList.add("active");
+//Creates a List of all buttons with class name "removebtn"
+var removeCartItemButtons = document.getElementsByClassName('removebtn')
+for (var i = 0; i< removeCartItemButtons.length; i++){
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event){  //when we click on the button, something happens
+        var buttonClicked = event.target //the actual item being clicked
+        buttonClicked.parentElement.remove() //selects the div surrounding it
+        updateCartTotal()
+    })
 }
 
-closeCart.onclick = () => {
-    cart.classList.remove("active");
+//Updates the total
+function updateCartTotal(){
+    var cartItemsContainer = document.getElementsByClassName('cart-Items-Container')[0]
+    var cartItems = cartItemsContainer.getElementsByClassName("cart-item")
+    for (var i=0; i<cartItems.length; i++){
+        var cartItem = cartItems[i] //gets the item itself
+    }
 }
+
+
+
+
+// //Cart
+
+// let cartIcon = document.querySelector('#cart-icon');
+// let cart = document.querySelector('.cart');
+// let closeCart = document.querySelector('#close-cart');
+
+
+// //Open and Close cart
+// cartIcon.onclick = () => {
+//     cart.classList.add("active");
+// }
+
+// closeCart.onclick = () => {
+//     cart.classList.remove("active");
+// }
